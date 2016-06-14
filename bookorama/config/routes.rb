4f75916e128fcaa95209dc297e30a73c5b2c4b6d
blackcoat77 <kimanovic@gmail.com>
2016-06-14 11:30:22 -0400

    The routes configuration and installing twitter bootstrap

--- bookorama/config/routes.rb
+++ bookorama/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+# add our Default route:
+match ':controller(/:action(/:id))', :via => [:get, :post]
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
