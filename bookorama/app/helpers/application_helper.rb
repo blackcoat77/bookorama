cf421e19b92443d13aef4c669ca6e668c5c647f3
blackcoat77 <kimanovic@gmail.com>
2016-06-25 22:12:57 -0400

    Add application helper for visible fields

--- bookorama/app/helpers/application_helper.rb
+++ bookorama/app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
+
+    # Method for showing error messages for form fields
     def error_messages_for(object)
         render(partial: 'application/error_messages', locals: { object: object })
+    end
+
+    # Method for showing in our admin layout if categories or books are visible.
+    # Instead of true false, it will add span tags for us, and we just need to style it with css
+    def status_tag(boolean, options = {})
+        options[:true_text] ||= ''
+        options[:false_text] ||= ''
+
+        if boolean
+            content_tag(:span, options[:true_text], class: 'status true')
+        else
+            content_tag(:span, options[:false_text], class: 'status false')
+
+        end
     end
 end
